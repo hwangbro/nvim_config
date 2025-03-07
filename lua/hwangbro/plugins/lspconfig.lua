d6d9e51d5ba806e017fa7b6a00046e940736ee9b
hwangbro <nickhwangbo@gmail.com>
2025-03-07 14:56:32 -0800

    switched to basedpyright

--- lua/hwangbro/plugins/lspconfig.lua
+++ lua/hwangbro/plugins/lspconfig.lua
@@ -136,7 +136,15 @@ return {
         local servers = {
             -- clangd = {},
             -- gopls = {},
-            pyright = {},
+            basedpyright = {
+                settings = {
+                    basedpyright = {
+                        analysis = {
+                            typeCheckingMode = "standard",
+                        },
+                    },
+                },
+            },
             csharp_ls = {},
             -- rust_analyzer = {},
             -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
