3a19ce6df0cbcf0609a8d3078360446fa3270c95
hwangbro <nickhwangbo@gmail.com>
2025-06-01 22:16:12 -0700

    add LspRestart

--- lua/hwangbro/lsp.lua
+++ lua/hwangbro/lsp.lua
@@ -50,6 +50,29 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
+local function restart_lsp(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local clients
+    if vim.lsp.get_clients then
+        clients = vim.lsp.get_clients({ bufnr = bufnr })
+    else
+        ---@diagnostic disable-next-line:deprecated
+        clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+    end
+
+    for _, client in ipairs(clients) do
+        vim.lsp.stop_client(client.id)
+    end
+
+    vim.defer_fn(function()
+        vim.cmd("edit")
+    end, 100)
+end
+
+vim.api.nvim_create_user_command("LspRestart", function()
+    restart_lsp()
+end, {})
+
 -- copied straight from blink
 local capabilities = {
     textDocument = {
